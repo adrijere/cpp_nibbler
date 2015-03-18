4ff432d6b663478973a02265b87aa32df04ec662
mathon_j <mathon_j@epitech.eu>
2015-03-18 23:14:21 +0100

    debut algo

--- class/Snake.cpp
+++ class/Snake.cpp
@@ -5,13 +5,52 @@
 ** Login   <mathon_j@mathonj>
 ** 
 ** Started on  Wed Mar 11 10:39:29 2015 Jérémy MATHON
-** Last update Wed Mar 11 10:42:57 2015 Jérémy MATHON
+** Last update Wed Mar 18 23:13:24 2015 Jérémy MATHON
 */
 
 #include	"Snake.hpp"
 
-Snake::Snake(int const &w, int const &h) : witdh(w), height(h)
+Snake::Snake(int const &w, int const &h) : width(w), height(h)
 {
-
+  this->snake.push_front((w % 2) - 2, h % 2);
+  this->snake.push_front((w % 2) - 1, h % 2);
+  this->snake.push_front(w % 2), h % 2);
+  this->snake.push_front((w % 2) + 1, h % 2);
 }
 
+const	std::list<t_snake>	Snake::&get_snake() const
+{
+  return (this->snake);
+}
+
+const	t_food			Snake::&get_food() const
+{
+  return (this->_food);
+}
+
+void				Snake::init_food(int const &x, int const &y)
+{
+  std::list<t_snake>::iterator	it;
+
+  this->_food.x = rand() % x;
+  this->_food.y = rand() % y;
+  for (it = this->snake.begin(); it != this->snake.end(); ++it)
+    {
+      if (it.x == this->_food.x && it.y == this->_food.y)
+	this->init_food(x, y);
+    }
+}
+
+void				Snake::check_food(int const &x, int const &y)
+{
+  if (this->get_snake().front().x == this->get_food().x && this->get_snake().front().x == this->get_food().y)
+    {
+      this->init_food(x, y);
+      this->snake.push_front(x, y);
+    }
+  else
+    {
+      this->snake.push_front(x, y);
+      this->snake.pop_back();
+    }
+}
--- header/Snake.hpp
+++ header/Snake.hpp
@@ -5,7 +5,7 @@
 ** Login   <mathon_j@mathonj>
 ** 
 ** Started on  Wed Mar 11 10:37:36 2015 Jérémy MATHON
-** Last update Wed Mar 11 10:42:50 2015 Jérémy MATHON
+** Last update Wed Mar 18 23:13:59 2015 Jérémy MATHON
 */
 
 #ifndef SNAKE_HPP_
@@ -13,14 +13,32 @@
 
 # include	<iostream>
 
-class	Snake : public IDisplay {
+typedef struct	s_snake
+{
+  int		x;
+  int		y;
+}		t_snake;
 
-  int		width;
-  int		height;
+typedef struct	s_food
+{
+  int		x;
+  int		y;
+}		t_food;
+
+class	Snake : public IDisplay {
+protected:
+  int			width;
+  int			height;
+  std::list<t_snake>	snake;
+  t_food		_food;
 
 public:
   Snake(int const &, int const &);
   ~Snake() {};
+  const std::list<t_snake>	&get_snake() const;
+  const	t_food			&get_food() const;
+  void				init_food(int const &, int const &);
+  void				check_food(int const &, int const &);
 };
 
 #endif /* !SNAKE_HPP_ */
