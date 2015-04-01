deca14a8416af7e61354a4ef2aa124e33bc60e84
hure_s <simon.hure@epitech.eu>
2015-04-01 13:25:20 +0200

    header file

--- ncurses/ncurses.hh
+++ ncurses/ncurses.hh
@@ -5,7 +5,7 @@
 // Login   <hure_s@epitech.net>
 // 
 // Started on  Mon Mar 30 11:33:18 2015 simon hure
-// Last update Mon Mar 30 16:07:20 2015 simon hure
+// Last update Wed Apr  1 12:42:04 2015 simon hure
 //
 
 #ifndef _NCURSES_HH_
@@ -13,6 +13,9 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <functional>
+#include <list>
 #include <ncurses.h>
 #include <unistd.h>
 #include "../header/IDisplay.hpp"
@@ -36,6 +39,9 @@ public:
   t_move move();
   void draw_border(WINDOW *screen);
   void handle_resize(WINDOW *std, WINDOW *game, int x, int y);
+  void snake_body(t_snake m);  
 };
+
+
 
 #endif
